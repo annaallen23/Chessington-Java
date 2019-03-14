19b393bc693dedae794069050ab95e3c3335cfdb
Anna Allen <Anna.kettlewell@yahoo.co.uk>
2019-03-14 15:02:41 +0000

    red 5

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -16,21 +16,33 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> moves = new ArrayList<Move>();
-        if (colour.equals(PlayerColour.WHITE) && from.getRow() !=0) {
+        if (colour.equals(PlayerColour.WHITE) && from.getRow() != 0) {
             if (from.getRow() == 6) {
                 moves.add(new Move(from, from.plus(-2, 0)));
             }
             if (board.get(from.plus(-1, 0)) == null) {
                 moves.add(new Move(from, from.plus(-1, 0)));
-
+            }
+            if (from.getCol() != 0 && board.get(from.plus(-1, -1)) != null && board.get(from.plus(-1, -1)).getColour().equals(PlayerColour.BLACK)) {
+                moves.add(new Move(from, from.plus(-1, -1)));
+            }
+            if (from.getCol() != 7 && board.get(from.plus(-1, +1)) != null && board.get(from.plus(-1, +1)).getColour().equals(PlayerColour.BLACK)) {
+                moves.add(new Move(from, from.plus(-1, +1)));
             }
         }
+
         if (colour.equals(PlayerColour.BLACK) && from.getRow() !=7) {
             if (from.getRow() == 1) {
                 moves.add(new Move(from, from.plus(+2, 0)));
             }
             if (board.get(from.plus(+1, 0)) == null) {
                 moves.add(new Move(from, from.plus(+1, 0)));
+            }
+            if (from.getCol() !=7 && board.get(from.plus(+1,+1)) != null && board.get(from.plus(+1,+1)).getColour().equals(PlayerColour.WHITE)){
+                moves.add(new Move(from, from.plus(+1,+1)));
+            }
+            if (from.getCol() !=0 && board.get(from.plus(+1,-1)) != null && board.get(from.plus(+1,-1)).getColour().equals(PlayerColour.WHITE)){
+                moves.add( new Move(from, from.plus(+1,-1)));
             }
         }
             return new ArrayList<>(moves);
