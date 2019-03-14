fc61aacabd671122a3ca017685760fb1913f873d
Anna Allen <Anna.kettlewell@yahoo.co.uk>
2019-03-14 14:04:41 +0000

    cherry-pick red-3

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -16,8 +16,27 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> moves = new ArrayList<Move>();
-        if (colour.equals(PlayerColour.WHITE))moves.add(new Move(from, from.plus(-1, 0)));
-        if (colour.equals(PlayerColour.BLACK))moves.add(new Move(from, from.plus(1, 0)));
-        return new ArrayList<>(moves);
+        if (colour.equals(PlayerColour.WHITE)) {
+            if (from.getRow() == 6) {
+                moves.add(new Move(from, from.plus(-2, 0)));
+            }
+            if (board.get(from.plus(-1, 0)) == null) {
+                moves.add(new Move(from, from.plus(-1, 0)));
+            }
+
+        } else if (colour.equals(PlayerColour.BLACK)) {
+            if (from.getRow() == 1) {
+                moves.add(new Move(from, from.plus(+2, 0)));
+            }
+            if (board.get(from.plus(+1, 0)) == null) {
+                moves.add(new Move(from, from.plus(+1, 0)));
+            }
+        }
+            return new ArrayList<>(moves);
+        }
     }
-}
+
+
+
+
+
